6c84c208a4cb862199143356ee3472ad9ca706ff
Yuv local <billiondollarone07@gmail.com>
2023-02-11 11:33:42 +0530

    - Astro setup

---
+++ src/scripts/script.js
@@ -0,0 +1,76 @@
+import barba from '@barba/core';
+import {
+    gsap
+} from "gsap";
+
+
+
+const pageTransition = () => {
+    var timeline = gsap.timeline();
+
+    timeline.to("header", {
+        zIndex: 1
+    });
+
+    timeline.to(".page-transition", {
+        duration: 1,
+        width: "100%",
+        left: "0%"
+    });
+
+    timeline.to(".page-transition", {
+        duration: .8,
+        width: "100%",
+        left: "100%",
+        delay: .3
+    });
+
+    timeline.set(".page-transition", {
+        left: "-100%"
+    });
+}
+
+const mainAnimation = () => {
+    var timeline = gsap.timeline();
+
+    timeline.from(".home h1, .home h2, .desc, nav", {
+        duration: 1,
+        y: 30,
+        opacity: 0,
+        stagger: {
+            amount: .4
+        },
+        delay: .8
+    });
+}
+
+const delay = (n) => {
+    n = n || 2000;
+    return new Promise((done) => {
+        setTimeout(() => {
+            done();
+        }, n);
+    })
+}
+
+barba.init({
+    sync: true,
+    transitions: [{
+        async leave(data) {
+            const done = this.async();
+            pageTransition();
+            await delay(1000);
+            done();
+        },
+
+        async enter(data) {
+            mainAnimation();
+        },
+
+        async once(data) {
+            mainAnimation();
+        }
+    }]
+});
+
+
