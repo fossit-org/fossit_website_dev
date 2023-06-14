2a381edccf84c3c13765caadaf639eab92e172bf
Manish <manishsuresh993@gmail.com>
2023-06-14 22:56:38 +0530

    front-page(intro box) and nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,42 @@
+import { A } from "@solidjs/router";
+import { animate } from "motion";
+
 
 const Nav = () => {
-    
+    return(
+        <>
+            <nav className="flex w-screen max-w-[1920px] h-max font-archivo justify-between p-8">
+            <div class="flex gap-16 justify-center items-center">
+                <A href="/" class="text-2xl font-bold border-l-8 pl-2 border-black">FOSSIT</A>
+                <div class="gap-4 hidden md:flex">
+                    <A href="/learn" class="btn">LEARN</A>
+                    <A href="/about" class="btn">ABOUT US</A>
+                </div>
+            </div>
+            <div>
+                <button class="btn hidden md:flex">CONTRIBUTE</button>
+            </div>
+            <button onClick={() => {
+                // document.querySelector(".menu").classList.toggle("flex");
+                // document.querySelector(".menu").classList.toggle("hidden");
+                animate(document.querySelector(".menu"), {display:"flex", ease: "easeInOut"}, {duration:.1})
+                animate(document.querySelector(".menu"), {top:["-300px", "0px"], ease: "easeInOut"}, {duration:.7})
+            }} class="btn md:hidden">MENU</button>
+            
+        </nav>
+        <div class="flex menu flex-col gap-8 w-screen p-16 pb-0 justify-center items-center absolute top-[-400px] bg-black font-archivo text-white">
+            <A href="/learn" class="">LEARN</A>
+            <A href="/about" class="">ABOUT US</A>
+            <button class="">CONTRIBUTE</button>
+            <button onClick={() => {
+                animate(document.querySelector(".menu"), {top:["0", "-300px"], ease: "easeInOut"}, {duration:.7})
+                animate(document.querySelector(".menu"), {display:"hidden", ease: "easeInOut"}, {duration:.1})
+                // document.querySelector(".menu").classList.toggle("flex");
+                // document.querySelector(".menu").classList.toggle("hidden");
+            }} class="w-screen p-2 bg-cyellow text-cblack font-black">CLOSE</button>
+        </div>
+        </>
+    )
 }
+
+export default Nav;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/pages/*.{js, jsx, ts, tsx}", "./index.html"],
+  content: ["./src/*/*.jsx", "./src/components/*.jsx", "./index.html"],
   theme: {
     extend: {
       colors: {
-        red: {
-          light: "#A72608",
-          dark: "#FF3E00",
-        },
-        yellow:{
-          light:"#E1CA96",
-          dark:"#F2A900"
-        },
-        blue:{
-          light:"#C2F4FF",
-          dark:"#0B4F6C"
-        },
-        page:{
-          light:"#E5F0F0",
-          dark:"#031426"
-        },
-        black:{
-          light:"#031426",
-          dark:"#000000"
-        }
-      }
+        cred:"#A72608",
+        cyellow:"#E1CA96",
+        cblue:"#C2F4FF",
+        page:"#E5F0F0",
+        cblack:"#031426",
+        cwhite:"#ccc",
+      },
+      fontFamily:{
+        poppins:['Poppins', 'sans-serif'],
+        montserrat:['Montserrat', 'sans-serif'],
+        archivo:['Archivo', 'sans-serif']
+      },
+      
     },
   },
   darkMode: "class",
